2c3b764c981f01d0656de5df360fe601d66aba29
Cegax <47731752+Cegax@users.noreply.github.com>
2024-02-09 16:28:40 -0600

    Delete articulos directory

--- articulos/src/main/java/com/example/articulos/Iinventario.java
+++
@@ -1,9 +0,0 @@
-package com.example.articulos;
-
-import javax.persistence.*;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface Iinventario extends CrudRepository<Inventario, Integer> {
-    
-}
--- articulos/src/main/java/com/example/articulos/IservicioPrestamo.java
+++
@@ -1,7 +0,0 @@
-package com.example.articulos;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IservicioPrestamo extends CrudRepository<ServicioPrestamo, Integer>{
-
-}
