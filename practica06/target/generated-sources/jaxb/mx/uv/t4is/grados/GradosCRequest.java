e69009579d27a2c750a0dde0dc52333b04721026
Cegax <47731752+Cegax@users.noreply.github.com>
2024-05-22 14:54:12 -0600

    practica06

---
+++ practica06/target/generated-sources/jaxb/mx/uv/t4is/grados/GradosCRequest.java
@@ -0,0 +1,61 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.05.08 a las 04:11:20 PM CST 
+//
+
+
+package mx.uv.t4is.grados;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="gradosC" type="{http://www.w3.org/2001/XMLSchema}double"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "gradosC"
+})
+@XmlRootElement(name = "GradosCRequest")
+public class GradosCRequest {
+
+    protected double gradosC;
+
+    /**
+     * Obtiene el valor de la propiedad gradosC.
+     * 
+     */
+    public double getGradosC() {
+        return gradosC;
+    }
+
+    /**
+     * Define el valor de la propiedad gradosC.
+     * 
+     */
+    public void setGradosC(double value) {
+        this.gradosC = value;
+    }
+
+}
